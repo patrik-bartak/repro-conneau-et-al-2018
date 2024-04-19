e13ce15f6abfcb21e3b3db1db13fdc765c584c9b
Patrik Bartak <patrik_bartak@outlook.com>
2024-04-19 17:40:24 +0200

    Add LRPlateau scheduler

--- src/models/nliclassifier.py
+++ src/models/nliclassifier.py
@@ -2,7 +2,7 @@ import lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.optim.lr_scheduler import ExponentialLR
+from torch.optim.lr_scheduler import ExponentialLR, ReduceLROnPlateau
 
 
 def get_mlp_layers_from_dims(mlp_dims):
@@ -23,6 +23,11 @@ class NLIClassifier(pl.LightningModule):
         self.lr = kwargs["lr"]
         self.lr_decay = kwargs["lr_decay"]
         self.save_hyperparameters(ignore=["embedding_mat"])
+        # To step both schedulers easily in the validation epoch
+        self.automatic_optimization = False
+        self.exp_scheduler = None
+        self.rlr_scheduler = None
+        self.step_val_accs = []
 
     def forward(self, u, v, l_u, l_v):
         # Embed token idxs
@@ -37,28 +42,49 @@ class NLIClassifier(pl.LightningModule):
         out = self.mlp(h)
         return out
 
-    def common_step(self, batch):
+    def common_step(self, batch, compute_acc=False):
         u, v, l_u, l_v, t = batch
-        out = self.forward(u, v, l_u, l_v)
-        return F.cross_entropy(out, t)
+        preds = self.forward(u, v, l_u, l_v)
+        loss = F.cross_entropy(preds, t)
+        if compute_acc:
+            acc = (torch.argmax(preds, dim=1) == t).float().mean()
+            return loss, acc
+        else:
+            return loss
 
     def training_step(self, batch, _):
+        opt = self.optimizers()
+        opt.zero_grad()
+
         loss = self.common_step(batch)
+
+        self.manual_backward(loss)
+        opt.step()
+
         self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         assert not loss.isnan().any(), "Some loss values are nan"
         return loss
 
     def validation_step(self, batch, _):
-        loss = self.common_step(batch)
+        loss, acc = self.common_step(batch, compute_acc=True)
+        # Save acc of validation step to compute epoch val acc later for early stopping scheduler
+        self.step_val_accs.append(acc)
         self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True)
-        # self.log("val_acc", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
         assert not loss.isnan().any(), "Some loss values are nan"
         return loss
 
+    def on_validation_epoch_end(self):
+        self.exp_scheduler.step()
+        # compute epoch val acc later for early stopping scheduler
+        epoch_val_acc = torch.stack(self.step_val_accs).mean()
+        self.rlr_scheduler.step(epoch_val_acc)
+        self.step_val_accs.clear()
+
     def test_step(self, batch, _):
-        loss = self.common_step(batch)
+        loss, acc = self.common_step(batch, compute_acc=True)
         self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
-        # self.log("test_acc", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_acc", acc, on_epoch=True, prog_bar=True, logger=True)
         assert not loss.isnan().any(), "Some loss values are nan"
         return loss
 
@@ -68,14 +94,6 @@ class NLIClassifier(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
-        # scheduler_1 = ReduceLROnPlateau(optimizer, "min", factor=0.2, patience=1)
-        scheduler_2 = ExponentialLR(optimizer, gamma=self.lr_decay)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler_2}
-        # return {
-        #     "optimizer": optimizer,
-        #     "lr_scheduler": {
-        #         "scheduler": scheduler_2,
-        #         "monitor": "val_loss",
-        #         "frequency": 1,
-        #     },
-        # }
+        self.exp_scheduler = ExponentialLR(optimizer, gamma=self.lr_decay)
+        self.rlr_scheduler = ReduceLROnPlateau(optimizer, "max", factor=0.2, patience=1)
+        return optimizer

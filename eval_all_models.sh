b4ec9f7b2afb7212caa681dc1f3036438ac9b441
Patrik Bartak <patrik_bartak@outlook.com>
2024-04-22 20:06:03 +0200

    Update eval script

--- eval_all_models.sh
+++ eval_all_models.sh
@@ -1,4 +1,4 @@
-python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_me "checkpoint/<...>"
-python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_lstme "checkpoint/<...>"
-python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_blstme "checkpoint/<...>"
-python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_blstmpme "checkpoint/<...>"
+python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_me "checkpoint/real_me_train/final.ckpt"
+python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_lstme "checkpoint/real_lstme_train/final.ckpt"
+python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_blstme "checkpoint/real_blstme_train/final.ckpt"
+python eval.py --checkpoint_dir checkpoint -b 64 -s 1 --run_name real_blstmpme "checkpoint/real_blstmpme_train/final.ckpt"
